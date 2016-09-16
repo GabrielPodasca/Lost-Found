8618ea4388ceb801bd71deebcf696ae7693ea6e4
GabrielPodasca <gabriel.podasca@gmail.com>
2016-09-16 23:15:45 +0300

    autocomplete part 1

--- app/src/main/java/com/cg/lostfoundapp/activities/LostOrFoundActivity.java
+++ app/src/main/java/com/cg/lostfoundapp/activities/LostOrFoundActivity.java
@@ -2,11 +2,9 @@ package com.cg.lostfoundapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -14,12 +12,11 @@ import android.widget.Toast;
 
 import com.cg.lostfoundapp.R;
 import com.cg.lostfoundapp.adapters.PlacesAutoCompleteAdapter;
-import com.cg.lostfoundapp.listeners.RecyclerItemClickListener;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.AutocompleteFilter;
 import com.google.android.gms.location.places.PlaceBuffer;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.model.LatLng;
@@ -34,72 +31,47 @@ public class LostOrFoundActivity extends AppCompatActivity implements GoogleApiC
     private String itemStatus;
 
     protected GoogleApiClient googleApiClient;
-    private static final LatLngBounds BOUNDS = new LatLngBounds(new LatLng(43,20),new LatLng(49,30));
+    private static final LatLngBounds BOUNDS = new LatLngBounds(
+            new LatLng(43.992815, 19.863281), new LatLng(48.195387, 30.761719));
 
-    private EditText autocompletePlaces;
-    private RecyclerView recyclerView;
-    private LinearLayoutManager linearLayoutManager;
+    private AutoCompleteTextView autocompletePlaces;
+
     private PlacesAutoCompleteAdapter autoCompleteAdapter;
     ImageView deleteImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         buildGoogleApiClient();
+
         setContentView(R.layout.activity_lost_or_found);
 
         initComponents();
 
-        linearLayoutManager = new LinearLayoutManager(this);
-
-        recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerView.setAdapter(autoCompleteAdapter);
-
         deleteImage.setOnClickListener(this);
 
-        autocompletePlaces.addTextChangedListener(new TextWatcher(){
-            public void onTextChanged(CharSequence s, int start, int before, int count){
-                if (!s.toString().equals("") && googleApiClient.isConnected()) {
-                    autoCompleteAdapter.getFilter().filter(s.toString());
-                }else if(!googleApiClient.isConnected()){
-                    Toast.makeText(getApplicationContext(), "Google API not connected",Toast.LENGTH_SHORT).show();
-                }
-            }
 
-                public void beforeTextChanged(CharSequence s, int start, int count, int after){}
 
-                public void afterTextChanged(Editable s){}
-            });
 
-        recyclerView.addOnItemTouchListener(
-                new RecyclerItemClickListener(this, new RecyclerItemClickListener.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(View view, int position) {
-                        final PlacesAutoCompleteAdapter.PlaceAutocomplete item = autoCompleteAdapter.getItem(position);
-                        final String placeId = String.valueOf(item.placeId);
-
-                        PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi.getPlaceById(googleApiClient, placeId);
-                        placeResult.setResultCallback(new ResultCallback<PlaceBuffer>() {
-                            @Override
-                            public void onResult(PlaceBuffer places) {
-                                if(places.getCount()==1){
-                                    //handle click on certain place
-                                    String placeName = places.get(0).getName().toString();
-                                    String placeAdress = places.get(0).getAddress().toString();
-                                    autocompletePlaces.setText(placeName+", "+placeAdress);
-                                }else{
-                                    Toast.makeText(getApplicationContext(),"OOPs!!! Something went wrong...",Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-                    }
-                })
-        );
 
     }
 
     private void initComponents(){
         iFoundEditText = (EditText) findViewById(R.id.iFoundEditText);
+
+        autocompletePlaces = (AutoCompleteTextView)findViewById(R.id.autocompletePlaces);
+        deleteImage=(ImageView)findViewById(R.id.cross);
+
+        AutocompleteFilter.Builder builder = new AutocompleteFilter.Builder();
+        builder.setTypeFilter(AutocompleteFilter.TYPE_FILTER_ADDRESS);
+        AutocompleteFilter autocompleteFilter = builder.build();
+
+        autoCompleteAdapter =  new PlacesAutoCompleteAdapter(
+                this, googleApiClient, BOUNDS, autocompleteFilter
+        );
+
+        autocompletePlaces.setAdapter(autoCompleteAdapter);
         whenLostText = (EditText) findViewById(R.id.whenTxt);
         descriptionLostText = (EditText) findViewById(R.id.descriptionText);
         postLostButton = (Button) findViewById(R.id.postButton);
@@ -109,10 +81,7 @@ public class LostOrFoundActivity extends AppCompatActivity implements GoogleApiC
             itemStatus = extras.getString("itemStatus");
         }
 
-        autocompletePlaces = (EditText)findViewById(R.id.autocompletePlaces);
-        deleteImage=(ImageView)findViewById(R.id.cross);
-        autoCompleteAdapter =  new PlacesAutoCompleteAdapter(this, R.layout.searchview_adapter, googleApiClient, BOUNDS, null);
-        recyclerView =(RecyclerView)findViewById(R.id.recyclerView);
+
     }
 
     protected synchronized void buildGoogleApiClient() {
--- app/src/main/java/com/cg/lostfoundapp/adapters/PlacesAutoCompleteAdapter.java
+++ app/src/main/java/com/cg/lostfoundapp/adapters/PlacesAutoCompleteAdapter.java
@@ -1,17 +1,16 @@
 package com.cg.lostfoundapp.adapters;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Filter;
 import android.widget.Filterable;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.cg.lostfoundapp.R;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.Status;
@@ -22,138 +21,191 @@ import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.model.LatLngBounds;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 
-public class PlacesAutoCompleteAdapter extends RecyclerView.Adapter<PlacesAutoCompleteAdapter.PredictionHolder> implements Filterable {
+public class PlacesAutoCompleteAdapter
+        extends ArrayAdapter<PlacesAutoCompleteAdapter.PlaceAutocomplete> implements Filterable {
 
-    //private static final String TAG = "PlacesAutoCompleteAdapter";
-    private ArrayList<PlaceAutocomplete> resultList;
+    private static final String TAG = "PlaceAutocomplete";
+    /**
+     * Current results returned by this adapter.
+     */
+    private ArrayList<PlaceAutocomplete> resultList = new ArrayList<>();
+
+    /**
+     * Handles autocomplete requests.
+     */
     private GoogleApiClient googleApiClient;
+
+    /**
+     * The bounds used for Places Geo Data autocomplete API requests.
+     */
     private LatLngBounds bounds;
-    private AutocompleteFilter placeFilter;
 
-    private Context context;
-    private int layout;
+    /**
+     * The autocomplete filter used to restrict queries to a specific set of place types.
+     */
+    private AutocompleteFilter autocompleteFilter;
 
-    public PlacesAutoCompleteAdapter(Context context, int resource, GoogleApiClient googleApiClient,
-                                     LatLngBounds bounds, AutocompleteFilter filter){
-        this.context = context;
-        layout = resource;
+
+    private Filter filter;
+
+
+    public PlacesAutoCompleteAdapter(Context context,  GoogleApiClient googleApiClient,
+                                     LatLngBounds bounds, AutocompleteFilter autocompleteFilter) {
+        super(context, 0);
         this.googleApiClient = googleApiClient;
         this.bounds = bounds;
-        placeFilter = filter;
+        this.autocompleteFilter = autocompleteFilter;
     }
 
-    public void setBounds(LatLngBounds bounds) {
-        this.bounds = bounds;
+
+    @Override
+    public int getCount() {
+        return resultList.size();
     }
 
     @Override
-    public Filter getFilter() {
-        Filter filter = new Filter() {
-            @Override
-            protected FilterResults performFiltering(CharSequence constraint) {
-                FilterResults results = new FilterResults();
-                if (constraint != null) {
-                    resultList = getAutocomplete(constraint);
-                    if (resultList != null) {
-                        results.values = resultList;
-                        results.count = resultList.size();
-                    }
-                }
-                return results;
-            }
-
-            @Override
-            protected void publishResults(CharSequence constraint, FilterResults results) {
-                if (results != null && results.count > 0) {
-                    notifyDataSetChanged();
-                }
-            }
-        };
-        return filter;
-    }
-
-    private ArrayList<PlaceAutocomplete> getAutocomplete(CharSequence constraint) {
-        if (googleApiClient.isConnected()) {
-            PendingResult<AutocompletePredictionBuffer> results =
-                    Places.GeoDataApi.getAutocompletePredictions(
-                            googleApiClient, constraint.toString(), bounds, placeFilter
-                    );
-
-            AutocompletePredictionBuffer autocompletePredictions = results.await(60, TimeUnit.SECONDS);
-
-            final Status STATUS = autocompletePredictions.getStatus();
-            if (!STATUS.isSuccess()) {
-                Toast.makeText(context,"Error contacting API: " + STATUS.toString(),Toast.LENGTH_SHORT).show();
-                autocompletePredictions.release();
-                return null;
-            }
-
-            Iterator<AutocompletePrediction> iterator = autocompletePredictions.iterator();
-            ArrayList resultList = new ArrayList<>(autocompletePredictions.getCount());
-            while (iterator.hasNext()) {
-                AutocompletePrediction prediction = iterator.next();
-                resultList.add(new PlaceAutocomplete(prediction.getPlaceId(),prediction.getDescription()));
-            }
-
-            autocompletePredictions.release();
-
-            return resultList;
-        }
-
-        return null;
-    }
-
-    @Override
-    public PredictionHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View convertView = layoutInflater.inflate(layout, viewGroup, false);
-        PredictionHolder predictionHolder = new PredictionHolder(convertView);
-        return predictionHolder;
-    }
-
-    @Override
-    public void onBindViewHolder(PredictionHolder predictionHolder, final int i) {
-        predictionHolder.prediction.setText(resultList.get(i).description);
-    }
-
-    @Override
-    public int getItemCount() {
-        if(resultList != null) {
-            return resultList.size();
-        }else {
-            return 0;
-        }
-    }
-
     public PlaceAutocomplete getItem(int position) {
         return resultList.get(position);
     }
 
-    public class PredictionHolder extends RecyclerView.ViewHolder {
-        private TextView prediction;
-        private RelativeLayout row;
-        public PredictionHolder(View itemView){
-            super(itemView);
-            prediction = (TextView) itemView.findViewById(R.id.address);
-            row =(RelativeLayout)itemView.findViewById(R.id.predictedRow);
+
+    @Override
+    public View getView( int position, View convertView, ViewGroup parent ) {
+        ViewHolder holder;
+
+        if( convertView == null ) {
+            holder = new ViewHolder();
+            convertView = LayoutInflater.from( getContext() ).inflate( android.R.layout.simple_list_item_1, parent, false  );
+            holder.text = (TextView) convertView.findViewById( android.R.id.text1 );
+            convertView.setTag( holder );
+        } else {
+            holder = (ViewHolder) convertView.getTag();
         }
+
+        holder.text.setText( getItem( position ).description );
+
+        return convertView;
+    }
+
+    @Override
+    public Filter getFilter() {
+        if (filter == null) {
+             filter = new Filter() {
+                @Override
+                protected FilterResults performFiltering(CharSequence constraint) {
+                    FilterResults results = new FilterResults();
+                    // Skip the autocomplete query if no constraints are given.
+                    if (constraint != null) {
+
+                        if (googleApiClient == null || !googleApiClient.isConnected()) {
+                            Log.e(TAG, "Google API client is not connected for autocomplete query.");
+                            return null;
+                        }
+
+
+                        // Query the autocomplete API for the (constraint) search string.
+                        getAutocomplete(constraint.toString());
+                        if (resultList != null) {
+                            // The API successfully returned results.
+                            results.values = resultList;
+                            results.count = resultList.size();
+                        }
+                    }
+                    return results;
+                }
+
+                @Override
+                protected void publishResults(CharSequence constraint, FilterResults results) {
+                    if (results != null && results.count > 0) {
+                        // The API returned at least one result, update the data.
+                        notifyDataSetChanged();
+                    } else {
+                        // The API did not return any results, invalidate the data set.
+                        notifyDataSetInvalidated();
+                    }
+                }
+                @Override
+                public CharSequence convertResultToString(final Object resultValue) {
+                    return resultValue == null ? "" : ((PlaceAutocomplete) resultValue).description;
+                }
+            };
+
+        }
+        return filter;
+    }
+
+    private void getAutocomplete(String constraint) {
+
+        Log.i(TAG, "Starting autocomplete query for: " + constraint);
+
+
+        // Submit the query to the autocomplete API and retrieve a PendingResult that will
+        // contain the results when the query completes.
+
+        PendingResult<AutocompletePredictionBuffer> results =
+                Places.GeoDataApi
+                        .getAutocompletePredictions(googleApiClient, constraint,
+                                bounds, autocompleteFilter);
+
+        //Block and wait for at most 60s
+        // for a result from the API.
+        AutocompletePredictionBuffer autocompletePredictions = results
+                .await(60, TimeUnit.SECONDS);
+
+        // Confirm that the query completed successfully, otherwise return null
+        final Status status = autocompletePredictions.getStatus();
+        if (autocompletePredictions == null || !status.isSuccess()) {
+            Toast.makeText(getContext(), "Error contacting API: " + status.toString(),
+                    Toast.LENGTH_SHORT).show();
+            Log.e(TAG, "Error getting autocomplete prediction API call: " + status.toString());
+            autocompletePredictions.release();
+            return;
+        }
+
+        Log.i(TAG, "Query completed. Received " + autocompletePredictions.getCount()
+                + " predictions.");
+
+        // Copy the results into our own data structure, because we can't hold onto the buffer.
+        // AutocompletePrediction objects encapsulate the API response (place ID and description).
+
+        resultList.clear();
+
+        for( AutocompletePrediction prediction : autocompletePredictions ) {
+            //Add as a new item to avoid IllegalArgumentsException when buffer is released
+            resultList.add( new PlaceAutocomplete( prediction.getPlaceId(), prediction.getFullText(null) ) );
+        }
+
+        // Release the buffer now that all data has been copied.
+        autocompletePredictions.release();
+
+        return;
 
     }
 
+    /**
+     * Holder for Places Geo Data Autocomplete API results.
+     */
     public class PlaceAutocomplete {
+
         public CharSequence placeId;
         public CharSequence description;
+
         PlaceAutocomplete(CharSequence placeId, CharSequence description) {
             this.placeId = placeId;
             this.description = description;
         }
+
         @Override
         public String toString() {
             return description.toString();
         }
     }
 
+
+    /** static cache item view */
+    private static class ViewHolder {
+        TextView text;
+    }
 }
